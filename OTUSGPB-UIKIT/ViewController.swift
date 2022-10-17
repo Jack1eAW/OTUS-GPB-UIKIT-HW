d98c52bf8d8d49a6bf3fd062b2d3a7fcce7e3cde
Jack1eAW <alcheb2014@gmail.com>
2022-10-17 12:34:59 +0300

    First commit: Final Version

--- OTUSGPB-UIKIT/ViewController.swift
+++ OTUSGPB-UIKIT/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number = 0
+    var timerCounting: Bool = false
+    var timer: Timer = Timer()
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        timerCounting = true
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+            timer.invalidate()
+        }
+    
+    @objc func timerCounter() -> Void
+        {
+            number = number + 1
+            let time = secondsToHoursMinutesSeconds(seconds: number)
+            let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+            timerLabel.text = timeString
+        }
+        
+        func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int)
+        {
+            return ((seconds / 3600), ((seconds % 3600) / 60),((seconds % 3600) % 60))
+        }
+        
+        func makeTimeString(hours: Int, minutes: Int, seconds : Int) -> String
+        {
+            var timeString = ""
+            timeString += String(format: "%02d", hours)
+            timeString += " : "
+            timeString += String(format: "%02d", minutes)
+            timeString += " : "
+            timeString += String(format: "%02d", seconds)
+            return timeString
+        }
 }
 
